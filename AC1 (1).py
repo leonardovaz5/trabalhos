f4d57877fc077fbd18d63d58381951975716db82
leonardovaz5 <161666568+leonardovaz5@users.noreply.github.com>
2024-03-04 18:47:12 -0300

    AC1 (1).py

--- AC1 (1).py
+++ AC1 (1).py
@@ -1,6 +1,5 @@
 import math
 
-# Função para resolver equação do segundo grau
 def resolver_equacao_segundo_grau(a, b, c):
     delta = b**2 - 4*a*c
     raiz_delta = math.sqrt(delta)
@@ -8,14 +7,11 @@ def resolver_equacao_segundo_grau(a, b, c):
     x2 = (-b - raiz_delta) / (2*a)
     return x1, x2
 
-# Entrada dos coeficientes da equação
 a = float(input("Informe o parâmetro a da equação: "))
 b = float(input("Informe o parâmetro b da equação: "))
 c = float(input("Informe o parâmetro c da equação: "))
 
-# Resolução da equação
 raiz1, raiz2 = resolver_equacao_segundo_grau(a, b, c)
 
-# Exibição das raízes
 print("A primeira raiz da equação é", raiz1)
 print("A segunda raiz da equação é", raiz2)
