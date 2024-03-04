ee25b9b46174408c86992cc8e381a14eaf075a37
leonardovaz5 <161666568+leonardovaz5@users.noreply.github.com>
2024-03-04 18:47:52 -0300

    Update AC1 (2).py

--- AC1 (2).py
+++ AC1 (2).py
@@ -1,7 +1,6 @@
-ano = int(input("Informe um ano: ")) # Solicita ao usuário que insira um ano
+ano = int(input("Informe um ano: ")) 
 
-# Verifica se o ano é bissexto e imprime o resultado
 if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
-    print(ano, "é um ano bissexto")  # Se for bissexto, imprime que é bissexto
+    print(ano, "é um ano bissexto") 
 else:
-    print(ano, "não é um ano bissexto")  # Se não for bissexto, imprime que não é bissexto
+    print(ano, "não é um ano bissexto") 
